26f3ff5fe9af254293e31a19e31d9432bd72acdf
yin palacios <palaciosjeananderson@gmail.com>
2023-06-19 18:56:08 -0500

    carga del proyecto a github

---
+++ js/cities.js
@@ -0,0 +1,26 @@
+var cities = [
+    {
+        "name": "Armenia",
+        "code": 1
+    },
+    {
+        "name": "Barranquilla",
+        "code": 2
+    },
+    {
+        "name": "Bogota",
+        "code": 3
+    },
+    {
+        "name": "cartagena",
+        "code": 4
+    },
+    {
+        "name": "Medellin",
+        "code": 5
+    },
+    {
+        "name": "Valledupar",
+        "code": 6
+    }
+];
